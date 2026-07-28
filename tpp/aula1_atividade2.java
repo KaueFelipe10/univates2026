ca896cf72ac28fb99c879d8ea8383b35dc446762
Kauê Felipe Valesan <kauefelipe@MacBook-Air-de-Kaue.local>
2026-07-28 11:16:36 -0300

    feat: segunda atividade java

--- tpp/aula1_atividade2.java
+++ tpp/aula1_atividade2.java
@@ -1,1 +1,35 @@
 /*2) implementar um programa que recebe um número inteiro e depois informe: quantos divisores o número tem, se o número é par ou ímpar e se o número é primo.*/
+
+import java.util.Scanner;
+
+public class aula1_atividade2 {
+
+    public static void main(String[] args) {
+        
+        Scanner entrada = new Scanner(System.in);
+        System.out.print("Digite um número inteiro: ");
+        int numero = entrada.nextInt();
+
+        System.out.println("Número digitado: " + numero);
+
+        if (numero % 2 == 0) {
+        System.out.println("Par");
+        } else {
+        System.out.println("Ímpar");
+        }
+
+        int quantidadeDivisores = 0;
+        for (int i = 1; i <= numero; i++) {
+            if (numero % i == 0) {
+                quantidadeDivisores++;
+            }
+        }
+        System.out.println("Divisores: " + quantidadeDivisores);
+
+        if (quantidadeDivisores == 2) {
+        System.out.println("Primo.");
+        } else {
+        System.out.println("Não é primo.");
+        }
+    }
+    }
